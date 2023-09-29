ad90e4aa6d56a56fa0a85703d0a2796a437bb8e9
AshleyLivingstoneTAC <michael.mcdonald@mymail.champlain.edu>
2023-09-29 14:17:32 -0400

    Update maze.cpp

--- courses/artificialintelligence/assignments/maze/maze.cpp
+++ courses/artificialintelligence/assignments/maze/maze.cpp
@@ -1,5 +1,95 @@
-// add your imports here
+#include <fstream>
+#include <vector>
+vector<bool> vert, hor, visit;
+struct point2D{ int x, y; };
+Point2D limit;
+void generateMap(Point2D limits)
+{
+    vert.resize(limits.x+1 * limits.y+1);
+    hor.resize(limits.x+1 * limits.y+1);
+    visit.resize(limits.x * limits.y);
+
+    for(int i = 0; i < visit.size; i++)
+    {
+        visit[i] = false;
+    }
+    for(int i = 0; i < hor.size; i++)
+    {
+        hor[i] = true;
+    }
+    for(int i = 0; i < vert.size; i++)
+    {
+        vert[i] = true;
+    }
+}
+
+
+
+enum class Direction
+{
+    UP = 0,
+    RIGHT,
+    DOWN,
+    LEFT;
+};
+void BreakWall(Point2D p, Direction d)
+{
+    switch(d){
+        case Direction::UP:
+            hor[limit.x*p.y+p.x] = false;
+            break;
+        case Direction::RIGHT:
+            vert[limit.x*p.y+p.x+1] = false;
+            break;
+        case Direction::DOWN:
+            hor[limit.x*(p.y+1)+p.x] = false;
+            break;
+        case Direction::LEFT:
+            vert[limit.x*p.y+p.x] = false;
+            break;
+    }
+}
+bool getWall(Point2D p, Direction d)
+{
+    switch(d){
+        case Direction::UP:
+           return hor[limit.x*p.y+p.x];
+            break;
+        case Direction::RIGHT:
+            return vert[limit.x*p.y+p.x+1];
+            break;
+        case Direction::DOWN:
+            return hor[limit.x*(p.y+1)+p.x];
+            break;
+        case Direction::LEFT:
+            return vert[limit.x*p.y+p.x];
+            break;
+    }
+}
+
+bool getVisit(Point2D p)
+{
+    return visit[limit.x*p.y+p.x];
+}
+
+void setVisit(Point2D p, bool state)
+{
+    visit[limit.x*p.y+p.x] = state;
+}
+
+vector<Point2D> getVisitable(point2D p)
+{
+    if(p.x < 0)
+        return true;
+    if (p.x > limit.x)
+        return true;
+
+}
 int main()
 {
-  // code here
+    int steps;
+    cin >> limit.x >> limit.y >> steps;
+    generateMap(limit);
+
 }
+
